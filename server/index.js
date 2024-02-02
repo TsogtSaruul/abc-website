64f88319b84370312d365f2593cf33e80b708bf5
TsogtSaruul <73455126+TsogtSaruul@users.noreply.github.com>
2024-02-02 14:25:40 +0800

    Update index.js

--- server/index.js
+++ server/index.js
@@ -12,11 +12,7 @@ const app = express();
 app.get('/', (req, res) => {
     res.send('APP IS RUNNING!')
 });
-app.use(cors({
-    origin: ["https://abc-website-client.vercel.app"],
-    methods: ["GET", "POST", "PATCH", "PUT", "DELETE"],
-    credentials: true
-}));
+app.use(cors());
 app.use(bodyParser.json({ limit: "30mb" }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use('/posts', postRoutes);

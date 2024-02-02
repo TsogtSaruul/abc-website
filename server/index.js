0d5094bc46a91d7b29306b4dc59fb16d240c313e
TsogtSaruul <73455126+TsogtSaruul@users.noreply.github.com>
2024-02-02 14:25:56 +0800

    Update index.js

--- server/index.js
+++ server/index.js
@@ -9,14 +9,15 @@ import mongoose from 'mongoose';
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('APP IS RUNNING!')
-});
+
 app.use(cors());
 app.use(bodyParser.json({ limit: "30mb" }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use('/posts', postRoutes);
 app.use('/user', userRoutes);
+app.get('/', (req, res) => {
+    res.send('APP IS RUNNING!')
+});
 
 
 dotenv.config();

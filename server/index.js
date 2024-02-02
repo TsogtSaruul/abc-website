911d24fd931e325adf7192f967545d0c85ee7d84
TsogtSaruul <73455126+TsogtSaruul@users.noreply.github.com>
2024-02-02 15:01:55 +0800

    Update index.js

--- server/index.js
+++ server/index.js
@@ -9,22 +9,31 @@ import mongoose from 'mongoose';
 
 const app = express();
 
+app.use(function (req, res, next) {
+    //Enabling CORS
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, 
+    Accept, x-client-key, x-client-token, x-client-secret, Authorization");
+      next();
+});
 app.use(cors({
     origin: ['https://abc-website-client.vercel.app'],
     methods: ["GET", "POST", "PATCH", "PUT", "DELETE"],
     credentials: true, // Optional depending on your requirements
 }));
-app.use((req, res, next) => {
-    req.setHeader('Access-Control-Allow-Origin', 'https://abc-website-client.vercel.app');
-    req.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    req.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-    req.setHeader('Access-Control-Allow-Credentials', true);
-    res.setHeader('Access-Control-Allow-Origin', 'https://abc-website-client.vercel.app');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-    res.setHeader('Access-Control-Allow-Credentials', true);
-    next();
-});
+
+// app.use((req, res, next) => {
+//     req.setHeader('Access-Control-Allow-Origin', 'https://abc-website-client.vercel.app');
+//     req.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+//     req.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+//     req.setHeader('Access-Control-Allow-Credentials', true);
+//     res.setHeader('Access-Control-Allow-Origin', 'https://abc-website-client.vercel.app');
+//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+//     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+//     res.setHeader('Access-Control-Allow-Credentials', true);
+//     next();
+// });
 app.use(bodyParser.json({ limit: "30mb" }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use('/posts', postRoutes);
